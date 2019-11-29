ffa243469f5220cf0ffb75db9ff41cafdfd55049
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2019-11-29 12:15:34 -0500

    Created header and footer.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header msg="Header"/>
+    <Main msg="Welcome to Symbiotic Juice site."/>
+    <Footer msg="Footer"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Footer,
+    Main
   }
 }
 </script>
